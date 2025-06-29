201eb5585d374ec4c35f9ad9f6eb394926c8df64
AlexRios123 <alexfabianomachado@gmail.com@gmail.com>
2025-06-29 08:24:09 -0300

    ajuste para deploy Render: porta e host

--- ola1.py
+++ ola1.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request, redirect, url_for
-
+import os
 app = Flask(__name__)
 
 USUARIO = "alex"
@@ -21,4 +21,5 @@ def bemvindo():
     return render_template("bemvindo.html")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
